82aa90d986000a557d60644bde56d0999b2b076c
sthiven <sthivendev@gmail.com>
2023-01-02 16:53:23 -0300

    :sparkles: Happy New Year!

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,12 @@ function App() {
   return (
     <div className="App">
     <a-scene>
-      <a-text value="HAPPY NEW YEAR!" color="gold" scale="2 2 1" position="-3 1.6 -2" rotation="0 45 0"></a-text>
-      <a-text value="<Code> 2023! <Code/>" color="green" scale="2 2 1" position="-3.3 1 -2" rotation="0 45 0"></a-text>
+      <a-box color="blue" position="-2.6 1.5 -2" width="0.5" height="2" rotation="0 30 0"></a-box>
+      <a-text value="HAPPY NEW YEAR!" color="gold" scale="4 4 1" position="-2 1.8 -2" rotation="0 30 0"></a-text>
+      <a-text value="<Code> 2023! <Code/>" color="green" scale="4 4 1" position="-2.3 1 -2" rotation="0 30 0"></a-text>
+      <a-box color="blue" position="6 1.5 -7" width="0.5" height="2" rotation="0 30 0"></a-box>
+      <a-box color="blue" position="3 0.5 -6" width="0.5" height="15" rotation="50 31 90"></a-box>
+      <a-box color="blue" position="3 2.5 -6" width="0.5" height="15" rotation="-50 31 90"></a-box>
       
       <a-text value="HAPPY NEW YEAR!" color="gold" scale="2 2 1" position="3 1.6 6" rotation="0 180 0"></a-text>
       <a-text value="<Code> 2023! <Code/>" color="green" scale="2 2 1" position="3.3 1 6" rotation="0 180 0"></a-text>
