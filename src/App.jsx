f6260edbaabfd75bcdd57de1f2859366101bb0b2
sthiven <sthivendev@gmail.com>
2023-01-02 23:59:38 -0300

    :sparkles: Happy New Year!

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
@@ -18,7 +15,7 @@ function App() {
       <a-text value="HAPPY NEW YEAR!" color="gold" scale="2 2 1" position="3 1.6 6" rotation="0 180 0"></a-text>
       <a-text value="<Code> 2023! <Code/>" color="green" scale="2 2 1" position="3.3 1 6" rotation="0 180 0"></a-text>
       <a-text value="so que atras" color="green" scale="2 2 1" position="2.5 0.4 6" rotation="0 180 0"></a-text>
-
+    
     </a-scene>
     </div>
   )
